9c27e072f0363c02d7767d43bb340cbfef2dc402
BorDo3010 <borisd3010@gmail.com>
2023-06-26 15:13:54 +0300

    Exam007 Треугольник 3 точки

---
+++ Examples/Exam005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "борис")
+{
+    Console.WriteLine("Приветствую тебя, о великолепный! ");
+
+}
+else
+{
+   Console.Write("Привет, "); 
+   Console.WriteLine(username);
+}
