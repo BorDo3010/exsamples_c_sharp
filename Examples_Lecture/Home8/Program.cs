e39440e6ceda8094814162bebd1018ffe28e4ca4
BorDo3010 <borisd3010@gmail.com>
2023-07-01 22:35:36 +0300

    Переименовал папку и создал новую

---
+++ Examples_Lecture/Home8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i=1;
+int j=0;
+int[] numbers = new int[N-1]; 
+Console.WriteLine("Четные значения в диапазоне от 1 до N: ");
+while(i<=N && j<(N-1)) 
+{
+    if(i%2 == 0)
+    {
+        numbers[j]=i; 
+        
+        Console.Write($"{numbers[j]}; ");       
+    }
+    i++;
+    j++;    
+}
