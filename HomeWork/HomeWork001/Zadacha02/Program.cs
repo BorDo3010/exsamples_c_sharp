653c7e4f6eb74773c677b8ca420a0057e17f5def
BorDo3010 <borisd3010@gmail.com>
2023-07-04 23:27:41 +0300

    Задача 02

---
+++ HomeWork/HomeWork001/Zadacha02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа
+и выдаёт, какое число большее, а какое меньшее.*/
+
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("max = ");
+
+if (a < b) {
+    Console.Write(b);
+} else {
+    Console.Write(a);
+}
+
+Console.WriteLine();
