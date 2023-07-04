96bcd07409cde4492d58928df72dd5d7b51f85eb
BorDo3010 <borisd3010@gmail.com>
2023-07-05 01:04:06 +0300

    Задача08

---
+++ HomeWork/HomeWork001/Zadacha08/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N..*/
+
+Console.Write("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+while (i <= N) 
+{  
+    Console.Write(i + ", " );
+    i += 2; 
+}
+
+Console.WriteLine();
