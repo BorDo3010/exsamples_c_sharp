5f70696cccd5d3fb8cfb6200ba5580aa5a68ec39
BorDo3010 <borisd3010@gmail.com>
2023-07-04 23:55:48 +0300

    Задача_4

---
+++ HomeWork/HomeWork001/Zadacha04/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("max = ");
+
+int m;
+
+if (a < b) {
+    m = b;
+} else {
+    m = a;
+}
+if (m < c) {
+    Console.Write(c);
+} else {
+    Console.Write(m);
+}
+
+Console.WriteLine();
