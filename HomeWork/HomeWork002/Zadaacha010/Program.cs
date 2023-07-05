fc8d9d32845d89e5c7125d4967817b2e261c6370
BorDo3010 <borisd3010@gmail.com>
2023-07-05 16:47:37 +0300

    примеры с семинара 1 Закоментированные

---
+++ HomeWork/HomeWork002/Zadaacha010/Program.cs
@@ -0,0 +1,41 @@
+﻿/*  Напишите программу, которая на вход
+ принимает два числа и проверяет, является
+ли первое число квадратом второго.*/
+
+// Console.Write("Введите первое число: ");
+// int firstDigit = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+// // firstDigit == secondDigit * secondDigit
+
+// if (firstDigit == secondDigit * secondDigit)
+// {
+//     Console.WriteLine("Число " + firstDigit + " является квадратом от числа " + secondDigit);
+// }
+// else
+// {
+//     Console.WriteLine("Число " + firstDigit + " НЕ является квадратом от числа " + secondDigit);  
+// }
+
+
+
+/*Напишите программу, которая на вход 
+принимает одно число (N), а на выходе 
+показывает все целые числа в промежутке от -N до N.*/
+
+// Console.Write("Введите число: ");
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 0)
+// {
+//     N = N * -1;
+// }
+// int negtiveN = N * -1;
+
+// while (negtiveN <= N)
+// {
+//     Console.Write(negtiveN + " ");
+//     negtiveN++;
+// }
