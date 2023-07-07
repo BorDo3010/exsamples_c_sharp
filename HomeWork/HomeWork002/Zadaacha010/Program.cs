cf0fe6a8a996443ba52f2c2c13e5b527c87e4592
BorDo3010 <borisd3010@gmail.com>
2023-07-08 01:30:12 +0300

    Задача 10 "осилил"

--- HomeWork/HomeWork002/Zadaacha010/Program.cs
+++ HomeWork/HomeWork002/Zadaacha010/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
 
 Console.WriteLine("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -9,10 +9,5 @@ int result = (number % 100) / 10;
 
 if (number >= 100 && number < 1000)
 {
-    Console.WriteLine(result);
-    else
-    {
-          Console.WriteLine);
-  
-    }
+    Console.WriteLine($"Вторая цифра числа : {number} - это {result}");
 }
