9e9ef6e857ae254e643abc0ebd768935ecb8ec05
BorDo3010 <borisd3010@gmail.com>
2023-09-05 02:32:30 +0300

    Задача 29

--- HomeWork/HomeWork004/Zadacha25/Program.cs
+++ HomeWork/HomeWork004/Zadacha25/Program.cs
@@ -1,41 +1,30 @@
 ﻿// Задача 25: Напишите который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.Write("Введите число: ");
 
-int limit = Convert.ToInt32(Console.ReadLine()); // Получили А 
-int sum = 0; // Суммы изначально нет
-// начало, условие, увеличение счетчика
-for (int i = 1; i <= limit; i++)
+double RaiseDegree(double number, int degree)
 {
-    sum += i;  // sum = sum + i;
-}
-Console.WriteLine($"Сумма чисел от 1 до {limit} = {sum}");
-
-
-int SumDigit(int numb)
-{
-    int sum = 0;
-
-    while (numb != 0)
-    {
-        sum = sum + numb % 10;
-        numb = numb / 10;
-    }
-    return sum;
+    double pow = Math.Pow(number, degree);
+    return pow;
 }
 
+Console.Clear();
 while (true)
 {
+    Console.WriteLine("-------------------------------------------------");
+    Console.WriteLine("Для выхода Ctrl+C");
+    Console.WriteLine("-------------------------------------------------");
     Console.Write("Введите число: ");
     string userNumberText = Console.ReadLine();
-    if (int.TryParse(userNumberText, out int userNumber))
+    Console.Write("Введите степень: ");
+    string userDegreeText = Console.ReadLine();
+    Console.Clear();
+    if (int.TryParse(userDegreeText, out int userDegree) && double.TryParse(userNumberText, out double userNumber))
     {
-        Console.Clear();
-        Console.WriteLine("Сумма цифр в числе {0} равняется {1}", userNumber, SumDigit(userNumber));
+        Console.WriteLine($"Сумма цифр в числе {userNumber} равняется {RaiseDegree(userNumber, userDegree)}");
     }
     else
-    { 
-    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    {
+        Console.WriteLine($"Не удалось распознать число {userNumberText}, попробуйте еще раз.");
     }
 }
---
+++ HomeWork/HomeWork004/Zadacha29/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] RandomArray(int limit)
+{
+    int[] array = new int[limit];
+    Random rand = new Random();
+    for (int i = 0; i < limit; i++)
+    {
+        array[i] = rand.Next(1, 1000);
+    }
+    return array;
+}
+
+void PrintRanomArray(int limit)
+{
+    int[] randArray = RandomArray(limit);
+    int count = randArray.Length;
+    Console.Write("[");
+
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1)
+        {
+            Console.Write($"{randArray[i]}");
+        }
+        else
+        {
+            Console.Write($"{randArray[i]}, ");
+        }
+    }
+    Console.Write("]");
+
+    Console.WriteLine();
+}
+
+Console.Clear();
+while (true)
+{
+    Console.WriteLine("-------------------------------------------------");
+    Console.WriteLine("Для выхода Ctrl+C");
+    Console.WriteLine("-------------------------------------------------");
+    Console.Write("Введите размер массива: ");
+    string userNumberText = Console.ReadLine();
+    Console.Clear();
+    if (int.TryParse(userNumberText, out int userNumber))
+    {
+        PrintRanomArray(userNumber);
+    }
+    else
+    {
+        Console.WriteLine($"Не удалось распознать число {userNumberText}, попробуйте еще раз.");
+    }
+}
