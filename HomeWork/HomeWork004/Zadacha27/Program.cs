3a46c03ad9beb68725801d1f9889dac18a75a6a5
BorDo3010 <borisd3010@gmail.com>
2023-09-05 01:03:44 +0300

    Задача 27

---
+++ HomeWork/HomeWork004/Zadacha27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+//  вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigit(int numb)
+{
+    int sum = 0;
+
+    while (numb != 0)
+    {
+        sum = sum + numb % 10;
+        numb = numb / 10;
+    }
+    return sum;
+}
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    string userNumberText = Console.ReadLine();
+    if (int.TryParse(userNumberText, out int userNumber))
+    {
+        Console.Clear();
+        Console.WriteLine("Сумма цифр в числе {0} равняется {1}", userNumber, SumDigit(userNumber));
+    }
+    else
+    { 
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+}
