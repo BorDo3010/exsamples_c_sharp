24c8b438c900cf2ee998e9e57a920cbdf1e1744b
BorDo3010 <borisd3010@gmail.com>
2023-09-05 21:47:19 +0300

    Добавил задача 49

--- Exam_Seminar/Semi007/Ex49/Program.cs
+++ Exam_Seminar/Semi007/Ex49/Program.cs
@@ -8,27 +8,48 @@
 // 8 4 2 4
 
 
-int[,] CreateMatrixArrayRndInt(int rows, int columns, int min, int max)
-{
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-          matrix [i, j] = rnd.Next(min, max + 1);
-        }
-    }
-    return matrix;
-}
-
-
-void PrintMatrix(int[,] matrix)
-{
-    Console.Write("[");
-   for (int j = 0; j < matrix.GetLength(1); j++)
-   {
-        Console.WriteLine($"{matrix[i, j], 5}");
-   } 
-}
+int[,] CreateMatrixSumIndexes(int rows, int columns,int min, int max) 
+{ 
+    int[,] matrix = new int[rows, columns]; 
+    Random rand = new Random(); 
+    for(int i = 0; i < rows; i++) 
+    { 
+        for(int j = 0; j < columns; j++) 
+        { 
+            matrix[i, j] = rand.Next(min,max+1); 
+        } 
+    } 
+    return matrix; 
+} 
+ 
+void PrintArray(int[,] arr) 
+{ 
+    int rows = arr.GetLength(0); 
+    int columns = arr.GetLength(1); 
+ 
+    for(int i = 0; i < rows; i++) 
+    { 
+        for(int j = 0; j < columns; j++) 
+        { 
+            Console.Write($"{arr[i, j], 5}"); 
+        } 
+        Console.WriteLine(); 
+    } 
+} 
+void EvenIndicesInPow(int[,] matrix) 
+{ 
+    for(int i = 0; i < matrix.GetLength(0); i+=2) 
+    { 
+        for(int j = 0; j < matrix.GetLength(1); j+=2) 
+        { 
+            matrix[i, j] *= matrix[i, j]; 
+        } 
+    } 
+} 
+ 
+int[,] matrix = CreateMatrixSumIndexes(3, 4,-5,5); 
+PrintArray(matrix); 
+EvenIndicesInPow(matrix); 
+Console.WriteLine(); 
+PrintArray(matrix);
